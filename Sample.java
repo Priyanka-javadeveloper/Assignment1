1d8cbffb825cca501b066b25ac12316a8d1080ba
priyanka-javadeveloper <priyankaacharya2402@gmail.com>
2026-05-04 10:05:36 +0530

    4-5-26

---
+++ Sample.java
@@ -0,0 +1,16 @@
+package com.corejava;
+
+ public class  Sample {
+class B{
+
+}
+
+	  public  static  void main(String[] tops) {
+	System.out.println("Hello");
+	System.out.println("java is best");
+
+	}
+ class A{
+	 
+ }
+ }
